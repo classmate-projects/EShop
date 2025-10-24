58255ab1218d4280e3a61a7c8f05e2ba41ce6829
Prabin Rag TK <prabinrag2340@gmail.com>
2025-10-24 12:56:42 +0400

    Removal of Platform service on shared project

--- EShop/Platforms/Android/MainActivity.cs
+++ EShop/Platforms/Android/MainActivity.cs
@@ -5,6 +5,15 @@ using Android.OS;
 namespace EShop
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter(
+    new[] { Android.Content.Intent.ActionView },
+    Categories = new[] {
+        Android.Content.Intent.CategoryDefault,
+        Android.Content.Intent.CategoryBrowsable
+    },
+    DataScheme = "eshop"
+)]
+
     public class MainActivity : MauiAppCompatActivity
     {
     }
