bdde6fc3efeecf2d50a1b57dafc5ed0a844af2e3
Prabin Rag TK <prabinrag2340@gmail.com>
2025-12-23 16:56:35 +0530

    Login Test

--- EShopNative/ViewModels/UserRoleEntryViewModel.cs
+++ EShopNative/ViewModels/UserRoleEntryViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using EShopNative.BaseLibrary;
 using EShopNative.Enums;
+using EShopNative.Pages;
+using EShopNative.Services;
 using System.Windows.Input;
 
 namespace EShopNative.ViewModels
 {
     public partial class UserRoleEntryViewModel : BaseViewModel
     {
+        private readonly AuthService _authService = new();
+        public string Email { get; set; }
+        public string Password { get; set; }
+
 
         [ObservableProperty]
         private AppViewState currentView;
@@ -54,6 +60,17 @@ namespace EShopNative.ViewModels
         }
         private async Task UserLogin(string role)
         {
+            var result = await _authService.LoginAsync(Email, Password);
+
+            if (result.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Success", "Login successful", "OK");
+                Application.Current.MainPage = new NavigationPage(new HomePage());
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", result.ErrorMessage, "OK");
+            }
 
         }
     }
